fcc671cc3b2938e7006637859de123e7a4f9034a
gogovoid <vcloud9@gmail.com>
2024-03-12 20:41:43 +0900

    v0.5 prime number

--- week02_prime_number.py
+++ week02_prime_number.py
@@ -4,9 +4,11 @@ if number < 2:
     count = 1
 else:
     count = 0
-    for i in range(2, number):  # -2 loop
+    for i in range(2, number):
         if number % i == 0:
             count = count + 1
+            break  # Exit the loop when the first divisor is found. Performance is improved when the input value is not a prime number.
+        print(i, end=" ")
 
 if count == 0:
     print(f"{number} is prime number~")
