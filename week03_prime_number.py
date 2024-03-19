9de372e3a58656ffd4488cf1fc563f5b0ad78441
gogovoid <vcloud9@gmail.com>
2024-03-19 20:56:02 +0900

    v1.4 prime number

--- week03_prime_number.py
+++ week03_prime_number.py
@@ -1,14 +1,25 @@
-start, end = list(map(int, input("Input start number : ").split()))
-
-for k in range(start, end+1):
-    is_prime_number = True
+def is_prime_number(n) -> bool:
+    """
+    prime number determination function
+    :param n: a positive integer
+    :return: Returns True if it is a prime number, and returns False if it is not a prime number.
+    """
+    #is_prime_number = True
     if k < 2:
-        is_prime_number = False
+        #is_prime_number = False
+        return False
     else:
         i = 2
-        while i*i <= k:
+        while i * i <= k:
             if k % i == 0:
-                is_prime_number = False
-                break
+                # is_prime_number = False
+                # break
+                return False
             i = i + 1
-        if is_prime_number: print(k, end=' ')
+        return True
+
+
+start, end = list(map(int, input("Input start & end number : ").split()))
+
+for k in range(start, end+1):
+        if is_prime_number(k): print(k, end=' ')
