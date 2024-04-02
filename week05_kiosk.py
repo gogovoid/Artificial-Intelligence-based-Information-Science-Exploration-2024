33f79ef8db8b011cf40b1b3114bfa5a2ee40aab8
gogovoid <vcloud9@gmail.com>
2024-04-02 20:20:15 +0900

    v0.0 kiosk

---
+++ week05_kiosk.py
@@ -0,0 +1,15 @@
+# ISHS CAFFE
+# Americano 1500, Latte 2500
+
+beverage = ["Americano", "Latte"]
+prices = [1500, 2500]
+
+while True:
+    menu = int(input("1) Americano   2) Latte    3) End order : "))
+    if menu == 3:
+        print("Exit program..")
+        break
+    elif menu == 1:
+        print("You ordered Americano coffee. The price is 1500 won.")
+    elif menu == 2:
+        print("You ordered a cafe latte. The price is 2500 won.")
