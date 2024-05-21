3a385a6486aef18c718fbbd27025f978f15a0b20
gogovoid <vcloud9@gmail.com>
2024-05-21 19:54:55 +0900

    v1.4 kiosk

--- week09_kiosk.py
+++ week09_kiosk.py
@@ -1,15 +1,15 @@
 # ISHS CAFFE
 # Americano 1500, Latte 2500
-def select_menu(index):
+def select_menu(key):
     """
     display menu, calculate total price and count quantity
-    :param index: index of list
+    :param key: key of dictionary
     :return: None
     """
     global total_price
-    print(f"You ordered {beverage[index]}. The price is {prices[index]} won.")
-    total_price = total_price + prices[index]
-    quantity[index] = quantity[index] + 1
+    print(f"You ordered {key}. The price is {beverage_price_quantity[key][0]} won.")
+    total_price = total_price + beverage_price_quantity[key][0]
+    beverage_price_quantity[key][1] = beverage_price_quantity[key][1] + 1
 
 
 beverage_price_quantity = {
@@ -33,11 +33,11 @@ while True:
         print("Your order has been accepted.")
         break
     elif menu == '1':
-        select_menu(0)
+        select_menu("americano coffee")
     elif menu == '2':
-        select_menu(1)
+        select_menu("caffe latte")
     elif menu == '3':
-        select_menu(2)
+        select_menu("iced tea")
     else:
         print(f"Menu number {menu} you ordered does not exist. Please choose from the menu.")
 
